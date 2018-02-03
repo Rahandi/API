c80180f0279650b5a86edef3179c59b2a7250f15
Unknown <rahandi16@mhs.if.its.ac.id>
2018-02-04 04:48:51 +0700

    failed

--- data/__init__.py
+++ data/__init__.py
@@ -2,5 +2,3 @@ from .InstagramAPI import InstagramAPI
 from .clarifaiapi import ClarifaiAPI
 from .openweathermap import owm
 from .ssweb import ScreenshotWeb
-from .joox import jooxAPI
-from PyQt5 import *
