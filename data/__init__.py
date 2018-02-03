e72652eb663d4a3110e46e710a9c74878480f954
Unknown <rahandi16@mhs.if.its.ac.id>
2018-02-04 04:23:18 +0700

    bismillah

--- data/__init__.py
+++ data/__init__.py
@@ -2,4 +2,4 @@ from .InstagramAPI import InstagramAPI
 from .clarifaiapi import ClarifaiAPI
 from .openweathermap import owm
 from .ssweb import ScreenshotWeb
-# from .joox import jooxAPI
+from .joox import jooxAPI
